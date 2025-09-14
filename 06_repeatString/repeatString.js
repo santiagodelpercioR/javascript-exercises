b4e613353cb10af9145fb730aa0ef3a72059aeb9
santiagodelpercioR <santiagodelpercio@gmail.com>
2025-09-14 20:35:36 -0300

    06 solved

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  if (num < 0) {
+    return "ERROR";
+  }
+  let i = 0;
+  let myString = "";
+  while (i < num) {
+    myString += string;
+    i++;
+  }
+  return myString;
 };
 
 // Do not edit below this line
