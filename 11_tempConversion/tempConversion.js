a43727e87f1064eb6efe12ac9c32413d51252997
santiagodelpercioR <santiagodelpercio@gmail.com>
2025-09-17 18:14:09 -0300

    09, 10 and 11 solved

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitNumber) {
+  return parseFloat(((fahrenheitNumber - 32) * (5 / 9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsiusNumber) {
+  return parseFloat((celsiusNumber * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
