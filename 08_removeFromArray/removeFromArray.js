99c12a7e801699aa356ad9bcb96b268087ad6bbb
santiagodelpercioR <santiagodelpercio@gmail.com>
2025-09-17 17:29:45 -0300

    07 and 08 solved

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...argsToRemove) {
+  let newArray = array.slice();
+  for (const toRemove of argsToRemove) {
+    let removeIndex = newArray.indexOf(toRemove);
+    while (newArray.includes(toRemove)) {
+      newArray = newArray.toSpliced(removeIndex, 1);
+    }
+    console.log(newArray);
+  }
+
+  return newArray;
 };
 
 // Do not edit below this line
